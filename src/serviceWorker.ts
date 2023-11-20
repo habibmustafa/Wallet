3d0ec36622abf84e6f468299b04d142f3d20442e
Habibullah Mustafazadeh <hmustafazadeh@its.gov.az>
2023-11-20 09:35:55 +0400

    update

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -44,7 +44,7 @@ export function register(config?: Config) {
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
-        // checkValidServiceWorker(swUrl, config);
+        checkValidServiceWorker(swUrl, config);
 
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
