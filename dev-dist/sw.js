e90cb70b7cfba19c7521ec4622e26000e807032f
Habibullah Mustafazadeh <hmustafazadeh@its.gov.az>
2023-11-22 15:24:14 +0400

    u[ate

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lp2n61ekb5o"
+    "revision": "0.rvph6du7i48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
