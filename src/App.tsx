c2ba71c9768304393040bcfa53604ad40889cdb5
Habibullah Mustafazadeh <hmustafazadeh@its.gov.az>
2023-11-22 11:44:38 +0400

    update index

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
 import MainRoutes from "./routes/MainRoutes.tsx";
 import "./App.scss";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const handleKeyboardOpen = () => {
-      const windowHeight = window.innerHeight;
-      const documentHeight = document.documentElement.clientHeight;
-      const keyboardHeight = windowHeight - documentHeight;
-      const rootElement = document.getElementById("root");
-      if (rootElement) {
-        rootElement.style.height = `calc(100vh - ${keyboardHeight}px)`;
-      }
-    };
-
-    window.addEventListener("resize", handleKeyboardOpen);
-
-    return () => {
-      window.removeEventListener("resize", handleKeyboardOpen);
-    };
-  }, []);
-
   return (
     <>
       <MainRoutes />
