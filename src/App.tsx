0eb90773c62f89c88d0515eb46b512c0796e920b
habibmustafa <hebibullahmustafazade@gmail.com>
2023-11-28 22:23:23 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import MainRoutes from "./routes/MainRoutes.tsx";
 import "./App.scss";
 
 function App() {
+  if("virtualKeyboard" in navigator) {
+    // @ts-ignore
+    var height = navigator.virtualKeyboard?.boundingRect?.height
+  }
+
+  
   return (
-    <>
+    <div>
+    height: {height}
       <MainRoutes />
-    </>
+    </div>
   );
 }
 
