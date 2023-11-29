81ec13b994ef20a40b56d1dfa11d92ba28324fb6
Habibullah Mustafazadeh <hmustafazadeh@its.gov.az>
2023-11-29 09:21:53 +0400

    grid templates

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,10 @@ import MainRoutes from "./routes/MainRoutes.tsx";
 import "./App.scss";
 
 function App() {
-  if("virtualKeyboard" in navigator) {
-    // @ts-ignore
-    var height = navigator.virtualKeyboard?.boundingRect?.height
-  }
-
-  
   return (
-    <div>
-    height: {height}
+    <>
       <MainRoutes />
-    </div>
+    </>
   );
 }
 
