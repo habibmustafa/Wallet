fa45d732c723b78e81d5547084b67c052f00c11e
Habibullah Mustafazadeh <hmustafazadeh@its.gov.az>
2023-12-01 13:46:12 +0400

    update

--- src/layouts/MainContent.tsx
+++ src/layouts/MainContent.tsx
@@ -1,19 +1,18 @@
-// import { ReactElement } from "react";
-import { Outlet } from "react-router-dom";
+import { ReactElement } from "react";
 import BottomTabScreen from "~/components/bottomTabScreen";
 import Container from "~/components/container";
 
-// interface IMainContent {
-//   children: ReactElement;
-// }
+interface IMainContent {
+  children: ReactElement;
+}
 
-const MainContent = () => {
+const MainContent = ({ children }: IMainContent) => {
   return (
     <Container
       className="main-content"
       style={{ display: "grid", gridTemplateRows: "minmax(0, 1fr) auto" }}
     >
-      <Outlet />
+      {children}
       <BottomTabScreen />
     </Container>
   );
