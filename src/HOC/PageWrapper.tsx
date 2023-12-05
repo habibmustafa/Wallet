d4de3044d609596234884003daf5743124fe6083
Habibullah Mustafazadeh <hmustafazadeh@its.gov.az>
2023-12-05 12:49:21 +0400

    fix

--- src/HOC/PageWrapper.tsx
+++ src/HOC/PageWrapper.tsx
@@ -1,6 +1,15 @@
-import { ComponentType, FC, ReactNode } from "react";
+import {
+  ComponentType,
+  FC,
+  // ReactNode
+} from "react";
 // import { FeedbackPopup } from "src/components";
-import { Navigate, Outlet, useNavigate, useLocation } from "react-router-dom";
+// import {
+//   Navigate,
+//   Outlet,
+//   useNavigate,
+//   useLocation
+// } from "react-router-dom";
 
 interface WrapperProps {
   permission?: boolean | null | undefined; // Define the 'open' prop with the correct type
@@ -8,27 +17,25 @@ interface WrapperProps {
 
 const PageWrapper = <T extends {}>(WrappedComponent: ComponentType<T>) => {
   const Wrapper: FC<WrapperProps & T> = (props) => {
-    let navigate = useNavigate();
+    // let navigate = useNavigate();
     const { permission } = props;
     // Render WrappedComponent only if 'permission' is truthy
     if (permission === null) {
       return null;
     }
-    const redirectHandle = () => {
-      navigate("/");
-    };
+    // const redirectHandle = () => {
+    //   navigate("/");
+    // };
     return permission ? (
       <WrappedComponent {...props} />
-    ) : (
-      null
-      // <FeedbackPopup
-      //   open={true}
-      //   message="Bu səhifəyə icazəniz yoxdur!"
-      //   status={2}
-      //   mainButtonText="Ana səhifəyə kecid"
-      //   handleClose={redirectHandle}
-      // />
-    );
+    ) : null;
+    // <FeedbackPopup
+    //   open={true}
+    //   message="Bu səhifəyə icazəniz yoxdur!"
+    //   status={2}
+    //   mainButtonText="Ana səhifəyə kecid"
+    //   handleClose={redirectHandle}
+    // />
   };
   return Wrapper;
 };
